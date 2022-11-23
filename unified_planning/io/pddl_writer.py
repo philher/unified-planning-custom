a5afaab101b2e65acd2581cde445bc461ab6d144
Philippe Hérail <pherail@laas.fr>
2022-11-23 11:49:47 +0100

    Fixed ordered tasks in htn part

--- unified_planning/io/pddl_writer.py
+++ unified_planning/io/pddl_writer.py
@@ -704,7 +704,7 @@ class PDDLWriter:
                 warnings.warn(
                     "assumes initial task network is totally ordered as in the list"
                 )
-                out.write("\n    (:ordered-tasks (and")
+                out.write("\n    :ordered-tasks (and")
                 for st in subtasks:
                     out.write(f"\n       ({self._get_mangled_name(st.task)}")
                     # TODO: Check if it possible for args not to be a Parameter, but another Fnode
@@ -712,7 +712,7 @@ class PDDLWriter:
                         f' {" ".join([self._get_mangled_name(p.object()) for p in st.parameters])}'
                     )
                     out.write(")")
-                out.write(")))\n")
+                out.write("))\n")
         out.write(" (:init")
         for f, v in self.problem.initial_values.items():
             if v.is_true():
